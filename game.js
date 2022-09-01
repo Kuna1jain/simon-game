a941fefabd9610d79735e484036c5e16e617902d
Kunal Jain <96196312+Kuna1jain@users.noreply.github.com>
2022-09-02 02:37:44 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,7 @@
+const pattern=[];
+function nextSequence(){
+    var random = Math.floor(Math.random()*4)=1;
+     const Colours=["red","blue","green","yellow"];
+    pattern.push(Colours[random]);
+    
+}
